457be4e0a3a95dbf7c5053e97bc54fbedcff3a35
Julio Alfaro <jalfaro.developer@gmail.com>
2015-11-15 19:38:12 -0600

    tarea con un cambio

--- Tarea#1.playground/Contents.swift
+++ Tarea#1.playground/Contents.swift
@@ -5,18 +5,19 @@ import UIKit
 var rango = 0...100
 
 for numero in rango {
-    
-    if numero % 5 == 0 {
-        println("\(numero)  Bingo!!!")
-    }
-    if numero  % 2 == 0 {
-        println ("\(numero)  par!!!")
-    }
-    if numero % 2 == 1 {
-        println ("\(numero)  impar!!!")
-    }
-    if (numero >= 30 && numero <= 40) {
-        println ("\(numero)  Viva Swift!!!")
+    if numero > 0 {
+        if numero % 5 == 0 {
+            println("\(numero)  Bingo!!!")
+        }
+        else if numero  % 2 == 0 {
+            println ("\(numero)  par!!!")
+        }
+        else if numero % 2 == 1 {
+            println ("\(numero)  impar!!!")
+        }
+        if (numero >= 30 && numero <= 40) {
+            println ("\(numero)  Viva Swift!!!")
+        }
     }
     
 }
