9247f5a0025129f95706affc10b0a5faf5910bee
Julio Alfaro <jalfaro.developer@gmail.com>
2015-11-15 19:30:36 -0600

    tarea finalizada

--- Tarea#1.playground/Contents.swift
+++ Tarea#1.playground/Contents.swift
@@ -2,15 +2,21 @@
 
 import UIKit
 
-var i = 1...12
+var rango = 0...100
 
-for numero in i {
-    println (numero)
-}
-
-switch i {
-case 2 :
-    print ("hola mundo")
-case 12:
-    print ("
+for numero in rango {
+    
+    if numero % 5 == 0 {
+        println("\(numero)  Bingo!!!")
+    }
+    if numero  % 2 == 0 {
+        println ("\(numero)  par!!!")
+    }
+    if numero % 2 == 1 {
+        println ("\(numero)  impar!!!")
+    }
+    if (numero >= 30 && numero <= 40) {
+        println ("\(numero)  Viva Swift!!!")
+    }
+    
 }
